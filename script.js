f9aae8ae9062fff5a70f48050180ae9bf427cbbf
Gustav Syrén <g.e.syren@gmail.com>
2023-09-26 15:52:57 +0200

    exempel

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello world!")
+
+let nameOne = "Sam"
+let nameTwo = "Ossian"
+let nameThree = "Gustav"
+
+console.log("Våra namn:", nameOne, nameTwo, nameThree)
+
+let year = 100
+const days = 44;
+console.log("En så kallad const variabel kan aldrig ändras efter att den har intierats. Men det kan en let variabel!")
+
+console.log("Tillsammans är vi över ", year, "gamla. Men ingen av oss är äldre än", days)
+console.log(33 + 44)
+console.log("Update:", year + days)
